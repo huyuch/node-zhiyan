1704dee7d3f919dee876396676307c92019490d2
huyuch <huyuch@163.com>
2015-08-04 16:46:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 /**
 *  zy.setConfig = {
 *    appKey: 'xxxx',
-*    token: 'xxxx',
-*    templateId: 'xxxx'
+*    token: 'xxxx'
 *  }
-*  zy.sendSms(mobile, code, callback)
+*  zy.sendSms(mobile, code, templateId, callback)
 */
 var request = require('request');
 var crypto =  require('crypto');
@@ -14,15 +13,15 @@ var zy = {
 	set setConfig(config) {
 		this.config = config;
 	},
-	sendSms: function(mobile, code, callback) {
+	sendSms: function(mobile, code, templateId, callback) {
 		var timestamp = Math.round(new Date().getTime()/1000);
-		var sign = crypto.createHash('md5').update(this.config.appKey + this.config.token + this.config.templateId + mobile + code + timestamp, 'utf8').digest("hex");
-    	var url = 'https://sms.zhiyan.net/sms/sms/single/' + this.config.appKey + '/' + this.config.token + '/' + this.config.templateId + '?timestamp=' + timestamp + '&sign=' + sign;
-    	var data = JSON.stringify({
-            mobile: mobile,
-            param: code,
-            extend: ''
-        });
+		var sign = crypto.createHash('md5').update(this.config.appKey + this.config.token + templateId + mobile + code + timestamp, 'utf8').digest("hex");
+    		var url = 'https://sms.zhiyan.net/sms/sms/single/' + this.config.appKey + '/' + this.config.token + '/' + templateId + '?timestamp=' + timestamp + '&sign=' + sign;
+    		var data = JSON.stringify({
+	            mobile: mobile,
+	            param: code,
+	            extend: ''
+	        });
 	    var opt = {
 	        rejectUnauthorized: false,
 	        url: url,
